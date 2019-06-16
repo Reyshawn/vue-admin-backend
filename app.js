b6ef87dc2397774c0316cbbfa8e5218fb23dc502
Reyshawn <reshawnchang@gmail.com>
2019-06-16 12:05:12 +0800

    Add DB and passport configuration.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const passport = require('passport')
+const mongoose = require('mongoose')
 const app = express()
 
+// Passport config
+require('./config/passport')(passport)
+
+// DB config
+const db = require('./config/keys').MongoURI
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected ...'))
+  .catch(err => console.log(err))
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res, next) => {
-  res.send('Hello expressssasdasd!')
+  res.send('Hello expre!')
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000...')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}...`)
 })
